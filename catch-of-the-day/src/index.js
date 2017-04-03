41fd5cebfabf47807b33b52f71f3bae1d507d299
Alex Simonian <alex.simonian@gmail.com>
2017-04-02 23:15:56 -0500

    Add components 🔥 🔥 🔥

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';			// imports the entire React package from react module
 import { render } from 'react-dom'; // import the render method from the react-dom
-
+import './css/style.css';			// import stylings from the style.css file
+import App from './components/App';
 import StorePicker from './components/StorePicker'; // import the StorePicker component from the directory
 
-render(<StorePicker/>, document.querySelector('#main')); // Mount the StorePicker component on the #main element
+render(<App/>, document.querySelector('#main')); // Mount the StorePicker component on the #main element
