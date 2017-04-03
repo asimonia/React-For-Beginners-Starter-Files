1efd88542626902e16dcc1bbf0155ec493a50e54
Alex Simonian <alex.simonian@gmail.com>
2017-04-02 19:39:19 -0500

    First React component

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+	render() {
+		return (
+			<form className="store-selector">
+				{ /* This is a legit comment */ }
+				<h2>Please Enter A Store</h2>
+				<input type="text" required placeholder="Store Name" />
+				<button type="submit">🔥🔥 Visit Store 🔥🔥</button>
+			</form>
+		)
+	}
+}
+
+export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,6 @@
-// let's go!
+import React from 'react';			// imports the entire React package from react module
+import { render } from 'react-dom'; // import the render method from the react-dom
+
+import StorePicker from './components/StorePicker'; // import the StorePicker component from the directory
+
+render(<StorePicker/>, document.querySelector('#main')); // Mount the StorePicker component on the #main element
