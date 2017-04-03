bfe601d38017b782fdd5e8ad0d5e868830912410
Alex Simonian <alex.simonian@gmail.com>
2017-04-03 14:57:13 -0500

    Loading data into state onClick

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -2,11 +2,16 @@ import React from 'react';
 import AddFishForm from './AddFishForm';
 
 class Inventory extends React.Component {
+	loadSamples() {
+
+	}
+
 	render() {
 		return (
 			<div>
 				<p>Inventory</p>
 				<AddFishForm addFish={this.props.addFish} />
+				<button onClick={this.props.loadSamples}>Load Sample Fishes</button>
 			</div>
 		)
 	}
