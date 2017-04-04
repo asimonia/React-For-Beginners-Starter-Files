790e615521a13619ed864c239ea9205ffcb7da08
Alex Simonian <alex.simonian@gmail.com>
2017-04-03 23:32:26 -0500

    Persisting state with firebase

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,10 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	// a connection to our Firebase database
+    apiKey: "AIzaSyD0VoBxTdsfgf2jSGf4t4BILYgf9w-PCwI",
+    authDomain: "catchoftheday-84e4b.firebaseapp.com",
+    databaseURL: "https://catchoftheday-84e4b.firebaseio.com",
+});
+
+export default base;
--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,7 @@ import Order from './Order';
 import Inventory from './Inventory';
 import Fish from './Fish';
 import sampleFishes from '../sample-fishes';
+import base from '../base';
 
 class App extends React.Component {
 	constructor() {
@@ -18,6 +19,18 @@ class App extends React.Component {
 			fishes: {},
 			order: {}
 		};
+	}
+
+	componentWillMount() {
+		this.ref = base.syncState(`${this.props.params.storeId}/fishes`
+			, {
+				context: this,
+				state: 'fishes'
+		});
+	}
+
+	componentWillUnmount() {
+		base.removeBinding(this.ref);
 	}
 
 	addFish(fish) {
@@ -45,8 +58,6 @@ class App extends React.Component {
 		this.setState({ order });
 	}
 
-
-
 	render() {
 		return (
 			<div className="catch-of-the-day">
@@ -60,7 +71,7 @@ class App extends React.Component {
 						}
 					</ul>
 				</div>
-				<Order />
+				<Order fishes={this.state.fishes} order={this.state.order} />
 				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
 			</div>
 		)
